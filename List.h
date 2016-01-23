53bc9867c00ebd68e1ac278bd138049946ab14ff
an.sokolov <sokolik_85@list.ru>
2016-01-23 14:44:27 +0600

    new project initialized. Created the first class for linkedLists project

---
+++ List.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   List.h
+ * Author: an.sokolov
+ *
+ * Created on 23 января 2016 г., 11:50
+ */
+
+#ifndef LIST_H
+#define	LIST_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* LIST_H */
+
---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: an.sokolov
+ *
+ * Created on 23 января 2016 г., 11:49
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
