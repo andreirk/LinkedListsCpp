62c4fdf462ddef9bc18203a42438029bbff2ba47
an.sokolov <sokolik_85@list.ru>
2016-01-23 14:46:02 +0600

    class list made in List.h file and add .gitignore

--- List.h
+++ List.h
@@ -8,6 +8,22 @@
 #ifndef LIST_H
 #define	LIST_H
 
+class List{
+private: 
+    
+   typedef struct node{
+        int date;
+        node * next;
+    }* nodePtr;
+    
+    nodePtr head;
+    nodePtr curr;
+    nodePtr temp;
+    
+public: 
+};
+
+
 #ifdef	__cplusplus
 extern "C" {
 #endif
